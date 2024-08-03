7336d670b8f95540af2ff94adc2e0314e3cd1796
Shaswat Raj <shaswatraj3@gmail.com>
2024-08-03 03:57:27 +0000

    feat: Add Terms of Service page

--- src/app/tos/page.js
+++ src/app/tos/page.js
@@ -3,7 +3,45 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      
+      <p><strong>Terms of Service</strong></p>
+<p>By accessing and using SopKit, you agree to be bound by these Terms of Service{` ("Terms")`}.</p>
+<ol>
+<li><strong>Free to Use</strong></li>
+</ol>
+<p>SopKit is a free online tool that can be used to streamline the JavaScript development workflow. There are no fees or charges associated with using SopKit.</p>
+<ol start="2">
+<li><strong>Acceptable Use</strong></li>
+</ol>
+<p>You agree to use SopKit only for lawful purposes. You agree not to use SopKit to:</p>
+<ul>
+<li>Upload, store, or transmit any content that is illegal, harmful, threatening, abusive, harassing, defamatory, obscene, hateful, or racially or ethnically offensive.</li>
+<li>Interfere with or disrupt the SopKit website or servers.</li>
+<li>Violate any applicable laws or regulations.</li>
+</ul>
+<ol>
+<li><strong>Disclaimer</strong></li>
+</ol>
+<p>SopKit is provided {`"as is"`} and without warranty of any kind, express or implied. The developers of SopKit disclaim all warranties, including any warranties of merchantability, fitness for a particular purpose, and non-infringement.</p>
+<ol start="4">
+<li><strong>Limitation of Liability</strong></li>
+</ol>
+<p>The developers of SopKit shall not be liable for any damages arising out of or related to your use of SopKit, including but not limited to direct, indirect, incidental, consequential, or punitive damages.</p>
+<ol start="5">
+<li><strong>Termination</strong></li>
+</ol>
+<p>The developers of SopKit reserve the right to terminate your access to SopKit at any time, for any reason, without notice.</p>
+<ol start="6">
+<li><strong>Changes to the Terms of Service</strong></li>
+</ol>
+<p>The developers of SopKit reserve the right to change these Terms of Service at any time. Your continued use of SopKit following the posting of any changes to the Terms of Service constitutes your acceptance of those changes.</p>
+<ol start="7">
+<li><strong>Governing Law</strong></li>
+</ol>
+<p>These Terms of Service shall be governed by and construed in accordance with the laws of the State of California, United States of America.</p>
+<ol start="8">
+<li><strong>Entire Agreement</strong></li>
+</ol>
+<p>These Terms of Service constitute the entire agreement between you and the developers of SopKit regarding your use of SopKit.</p>
     </div>
   )
 }
